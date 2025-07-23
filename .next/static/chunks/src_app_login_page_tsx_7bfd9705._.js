b1cee9bcf96735dc73387f27f3348c100976ee65
nehajhossb <132701331+nehajhossb@users.noreply.github.com>
2025-07-23 20:48:35 +0530

    Add .next/static/chunks/src_app_login_page_tsx_7bfd9705._.js 1753283915417

---
+++ .next/static/chunks/src_app_login_page_tsx_7bfd9705._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_8f8f0966._.js",
+  "static/chunks/src_2f7b1cdd._.js"
+],
+    source: "dynamic"
+});
