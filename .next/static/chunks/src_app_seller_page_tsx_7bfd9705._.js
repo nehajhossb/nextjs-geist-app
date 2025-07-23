6667644fb80c55edec9843944ef0df79fb5215bb
nehajhossb <132701331+nehajhossb@users.noreply.github.com>
2025-07-23 20:48:22 +0530

    Add .next/static/chunks/src_app_seller_page_tsx_7bfd9705._.js 1753283902615

---
+++ .next/static/chunks/src_app_seller_page_tsx_7bfd9705._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_dfb9eb29._.js",
+  "static/chunks/node_modules_2b2b21f0._.js"
+],
+    source: "dynamic"
+});
